9ee274e20a20f8ac561a4a2acfbb9b607f4cd02e
Silas P Ladislau <splhead@gmail.com>
2016-09-04 16:13:40 -0400

    testando

---
+++ src/resources/views/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<title>LLS Login</title>
+</head>
+<body>
+	<div class="container">
+
+	   <form action="" class="col-lg-4 col-lg-offset-4">
+	   		<h1>LLS</h1>
+	   		<labe for="usuario" class="sr-only">usuario/</labe>
+	   		<input type="text" name="usuario" id="usuario" class="form-control" />
+	   		<label for="senha" class="sr-only">senha</label>
+	   		<input type="password" name="senha" id="senha" class="form-control" />
+	   		<button class="btn btn-lg btn-primary btn-block">Entrar</button>
+	   </form>
+	</div>
+	<script src="js/jquery.min.js" type="text/javascript"/>
+	<script src="js/bootstrap.min.js" type="text/javascript"/>
+</body>
+</html>

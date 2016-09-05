d4c8c51879274f35677490c4c90e541fb0b0548a
Silas Pinho Ladislau <splhead@gmail.com>
2016-09-05 10:57:27 -0400

    Tela de login prototipada

--- src/resources/views/login.php
+++ src/resources/views/login.php
@@ -4,17 +4,18 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/login.css">
 	<title>LLS Login</title>
 </head>
 <body>
 	<div class="container">
 
 	   <form action="" class="col-lg-4 col-lg-offset-4">
-	   		<h1>LLS</h1>
+	   		<img src="images/logo.png" class="center-block">
 	   		<labe for="usuario" class="sr-only">usuario/</labe>
-	   		<input type="text" name="usuario" id="usuario" class="form-control" />
+	   		<input type="text" name="usuario" id="usuario" class="form-control" placeholder="usu&aacute;rio" required autofocus />
 	   		<label for="senha" class="sr-only">senha</label>
-	   		<input type="password" name="senha" id="senha" class="form-control" />
+	   		<input type="password" name="senha" id="senha" class="form-control" placeholder="senha" />
 	   		<button class="btn btn-lg btn-primary btn-block">Entrar</button>
 	   </form>
 	</div>
